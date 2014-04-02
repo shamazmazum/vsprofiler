6d505c6c84aec92a6c5ec1b945f32ceb847ba103
Vasily Postnicov <shamaz.mazum@gmail.com>
2014-04-02 19:15:43 +0400

    Fixed build

--- src/analizer/build.lisp
+++ src/analizer/build.lisp
@@ -19,10 +19,23 @@
   (let ((args #+sbcl (cdr sb-ext:*posix-argv*)
               #+clisp ext:*args*))
     (cond
-      ((= (length args) 2)
-       (print (vsanalizer:report (nth 0 args)
-                                 (nth 1 args)))
-       (terpri))
+      ((>= (length args) 2)
+       (let ((report (vsanalizer:analize (nth 0 args)
+                                         (nth 1 args)))
+             report-args)
+         (loop for i from 2 below (length args) by 2 do
+              (cond
+                ((and (string= "--sorting-method" (nth i args))
+                      (string= "self" (nth (1+ i) args)))
+                 (setq report-args (nconc (list :sorting-method :self) report-args)))
+                ((and (string= "--sorting-method" (nth i args))
+                      (string= "cumul" (nth (1+ i) args)))
+                 (setq report-args (nconc (list :sorting-method :cumul) report-args)))
+                (t
+                 (format t "Cannot understand option ~A. Invoke vsanalizer without parameters to see usage~%"
+                         (nth i args)))))
+         (print (apply #'vsanalizer:report report report-args))
+         (terpri)))
       (t
        (format t "Usage: vsanalizer prof.smpl prof.map~%"))))
   #+clisp (ext:quit 0))
